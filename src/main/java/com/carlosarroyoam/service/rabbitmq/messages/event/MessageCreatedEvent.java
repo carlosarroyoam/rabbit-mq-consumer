389d90eaceeee2f63480d063d8ca13c917d4d105
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2026-04-22 21:17:24 -0600

    Actualiza spring-boot a la versión 3.5.13

--- src/main/java/com/carlosarroyoam/service/rabbitmq/messages/event/MessageCreatedEvent.java
+++ src/main/java/com/carlosarroyoam/service/rabbitmq/messages/event/MessageCreatedEvent.java
@@ -1,8 +1,10 @@
 package com.carlosarroyoam.service.rabbitmq.messages.event;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class MessageCreatedEvent {
   private Integer id;
   private String content;
